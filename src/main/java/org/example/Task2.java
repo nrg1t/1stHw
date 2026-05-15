1fb9c90cba4774f8d7bd1a444d6ec3d1931a9d50
nrg1t <nino.rostobaia@gmail.com>
2026-05-15 18:46:35 +0400

    მესამე საკითხის ფაილის ატვირთვა და ფაილებში სავარჯიშოს პირობების გაწერა კომენტარის სახით

--- src/main/java/org/example/Task2.java
+++ src/main/java/org/example/Task2.java
@@ -2,15 +2,17 @@ package org.example;
 
 public class Task2 {
     public static void main(String[] args) {
-//       2. ტექსტის სიგრძის ლუწ-კენტობა
+//       2. შექმენით ცვლადი და გადაეცით თქვენთვის სასურველი ნებისმიერი
+//წინადადება და შეამოწმეთ, თუ ამ წინადადების სიგრძე იქნება ლუწი რიცხვი,
+//დაბეჭდეთ :"ლუწი", თუ კენტი მაშინ: "კენტი"
         String sentence = "დავაგვიანე დავალების დაწერა";
         int sentLength = sentence.length();
 
         if (sentLength % 2 ==0 ){
-            System.out.println("წინადადებაში სიმბოლოების რაოდენობა არის ლ უ წ ი");
+            System.out.println("ლუწი");
         }
         else {
-            System.out.println("წინადადებაში სიმბოლოების რაოდენობა არის კ ე ნ ტ ი");
+            System.out.println("კენტი");
         }
 
     }
---
+++ src/main/java/org/example/Task3.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Task3 {
+    public static void main(String[] args) {
+//        3. for -ით დაბეჭდეთ მასივში არსებული ყოველი ელემენტი, გარდა მეხუთე
+//პოზიციაზე მდგომისა
+        int[] numbers = {1, 3, 4, 22, 62, 42, 34, 74, 52, 35, 55};
+        for (int i = 0; i < numbers.length; i++) {
+            if (i != 4) {
+                System.out.println(numbers[i]);
+            }
+        }
+    }
+}
